2f9d02105cd9e51df3bd304f2a1a411fe3ccbc88
MortMo <mortimer_386@hotmail.com>
2019-10-08 17:38:18 +0200

    Opg 5 og deler av opg 6

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -132,10 +132,55 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }// end leggInn
     ///////////////////////////////////////////////////////////////////
 
+
+
+    /////////////////////////// Oppgave 5 /////////////////////////////////////////////////////
     @Override
     public void leggInn(int indeks, T verdi) {
-        throw new NotImplementedException();
+        if (verdi == null){
+            throw new NullPointerException();
+        }
+
+        if (indeks < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        if (indeks > (antall-1)){
+            throw new IndexOutOfBoundsException();
+        }
+
+
+        Node<T> newNode = new Node<T>(verdi);
+        if (hode == null ){ // Hvis Listen er Tom
+            hode = newNode;
+            hale = newNode;
+        }
+
+        else if (indeks == 0){
+            newNode.neste = hode;
+            hode.forrige = newNode;
+            hode = newNode;
+        }
+
+        else if (indeks == (antall - 1)){
+            newNode.forrige = hale;
+            hale.forrige = newNode;
+            hale = newNode;
+        }
+
+        else {
+            Node<T> nodeTemp = hode;
+            for (int i = 1; i < indeks; i++){
+                nodeTemp = nodeTemp.neste;
+                newNode.neste = nodeTemp.neste;
+                nodeTemp.neste = newNode;
+                newNode.forrige = nodeTemp;
+                newNode.neste.forrige = newNode;
+            }
+        }
+        antall++;
+        endringer++;
     }
+    ///////////// oppgave 5 slutt
 
     @Override
     public boolean inneholder(T verdi) {
@@ -193,15 +238,61 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         }// end oppdater
 
 
+
+    ///////////////////// Oppgave 6 /////////////////////////////////////
     @Override
     public boolean fjern(T verdi) {
+        if (verdi == null){
+            return false;
+        }
+        int a;
+        for(int i = 0; i < antall; i++){
+            Node (i)
+
+        }
+
+
         throw new NotImplementedException();
+
+
     }
 
     @Override
     public T fjern(int indeks) {
-        throw new NotImplementedException();
+
+        Node<T> q = finnNode(indeks);
+        Node<T> p = finnNode(indeks - 1);
+        Node<T> r = finnNode(indeks + 1);
+
+        if (q == null){
+            throw new IndexOutOfBoundsException();
+        }
+        if (indeks> (antall-1)){
+            throw new IndexOutOfBoundsException();
+        }
+        if(indeks<0){
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (p == null && r == null){
+            hode = null;
+            hale = null;
+        }
+        else if (p == null){
+            r.forrige = null;
+            hode = r;
+        }
+        else {
+            p.neste = r;
+            r.forrige = p;
+        }
+        endringer++;
+        antall--;
+
+        return (T) q;
+
     }
+    //////////////////oppgave 6 slutt
 
     @Override
     public void nullstill() {
