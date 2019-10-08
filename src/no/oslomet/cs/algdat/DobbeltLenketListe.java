8c03d5fdcebd071f9ea53e962409913d87d10ad4
MortMo <mortimer_386@hotmail.com>
2019-10-08 20:58:42 +0200

    Opg 6

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -265,16 +265,44 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         if (verdi == null){
             return false;
         }
-        int a;
-        for(int i = 0; i < antall; i++){
-            Node (i)
-
+        if (antall == 1){
+            hode = null;
+            hale = null;
+            return true;
         }
 
+        int a = -1;
+        Node g = hode;
+        for(int i = 0; i < antall; i++){
+            g = g.neste;
+            if(g.verdi == verdi){
+                a = i;
+                break;
+            }
+            return false;
+        }
+        Node<T> p = finnNode(a - 1);
+        Node<T> q = finnNode(a + 0);
+        Node<T> r = finnNode(a + 1);
 
-        throw new NotImplementedException();
-
-
+        if (a == -1){
+            return false;
+        }
+        if (a == 0){
+            r.forrige = null;
+            hode = r;
+            return true;
+        }
+        else if (a == (antall - 1)){
+            p.neste = null;
+            hale = p;
+            return true;
+        }
+        else{
+            p.neste = r;
+            r.forrige = p;
+            return true;
+        }
     }
 
     @Override
